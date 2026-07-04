cad832dd3e0a9257688ed07d41e914815f4d2ee9
Vastvik Sharma <vastviksharma8051@gmail.com>
2026-07-04 17:15:32 +0530

    Fix remaining navbar.module.css import casing

--- my-portfolio/src/components/Navbar/menuButtons.tsx
+++ my-portfolio/src/components/Navbar/menuButtons.tsx
@@ -1,4 +1,4 @@
-import styles from "./navbar.module.css";
+import styles from "./Navbar.module.css";
 
 export default function MenuButton() {
   return (
--- my-portfolio/src/components/Navbar/navLinks.tsx
+++ my-portfolio/src/components/Navbar/navLinks.tsx
@@ -1,4 +1,4 @@
-import styles from "./navbar.module.css";
+import styles from "./Navbar.module.css";
 
 const links = [
   "Home",
